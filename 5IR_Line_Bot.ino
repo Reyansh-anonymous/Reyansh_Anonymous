af730a960bbe83abcfe8bdd17e42ad77fe374bd1
Reyansh-anonymous <rmvpg1608@gmail.com>
2025-10-21 14:02:06 +0530

    Create 5IR_Line_Bot.ino
    This is a practice code for line following bot using 5 IR Sensors. Created for ProjectX

---
+++ 5IR_Line_Bot.ino
@@ -0,0 +1,58 @@
+int s1= A0; // Calling all sensors[autobots:)]
+int s2= A1;
+int s3= A2;
+int s4= A3;
+int s5= A4;
+const int left = 6;  
+const int right = 5; // for motors - 6 and 5 are PWm pins.
+int lefts;
+int rights; // motor speeds
+float s;
+float e1 = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(right, OUTPUT);
+  pinMode(left, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int S1 = analogRead(A0); // weight = -2, rightmost sensor
+  int S2 = analogRead(A1); // weight = -1, second to rightmost
+  int S3 = analogRead(A2); // weight = 0, middle
+  int S4 = analogRead(A3); // weight = 1, second to leftmost
+  int S5 = analogRead(A4); // weight = 2, leftmost sensor
+  int sum = (S1+S2+S3+S4+S5);
+  float val = 0;
+  
+
+  if (sum !=0){
+    val = float(S1*-2 + S2*-1 + S4*1 + S5*2)/float(sum);  // convert to float, as we get integer in normal division in C++
+  } // only if not dividing by 0
+  else{
+    val = 0;
+    s=0; // Go ahead, if entire surface is black
+  }
+  
+
+  s = s+val; // to count accumulated error
+  s = constrain(s,-100,100); // to limit accumulated error
+  float d =  val- e1; // to find change in error
+  e1 = val; 
+
+  float PID_val = 20*val + 0.05*s + 10*d; // PID calculation
+  lefts = 200+PID_val;
+  rights = 200-PID_val;
+  lefts = constrain(lefts, 0, 255);
+  rights = constrain(rights, 0, 255);
+  analogWrite(left, lefts );
+  analogWrite(right, rights);
+  
+  
+
+
+  Serial.println(val);
+  delay(20);
+}
