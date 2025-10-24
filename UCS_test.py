86836c8b160602a2f83c46aa977225d3f3002d29
Reyansh-anonymous <rmvpg1608@gmail.com>
2025-10-24 14:34:55 +0530

    Update UCS_test.py

--- UCS_test.py
+++ UCS_test.py
@@ -26,7 +26,7 @@ hq.heappush(heap, (maze[y,x],(y,x)))
 def valid(x,y):
       return 0 <= x < maze.shape[1] and 0 <= y < maze.shape[0]
 while heap:
-      cost, (y,x) = hq.heappop(heap)
+      cost, (y,x) = hq.heappop(heap) # Popping the cumulated cost  and current step that we are on
       if (y,x) == goal:
             print("Goal Reached, at the expense of:", cost)
             break
@@ -38,9 +38,9 @@ while heap:
       
       for dx, dy in direct:
             ny, nx = y+dy, x+dx
-            
-            if valid(nx,ny) and not vis[ny,nx] and maze[ny,nx]!=-1:
-                  hq.heappush(heap,(cost+maze[ny,nx],(ny,nx)))
+            if valid(nx,ny):
+                if not vis[ny,nx] and maze[ny,nx]!=-1:
+                    hq.heappush(heap,(cost+maze[ny,nx],(ny,nx))) # Pushing the least cost value to heap and moving on to next step, for popping in the while loop
 vis[4,4]= True
 print(vis) # For path
       
